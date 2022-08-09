c53c9008ad1a1ce6f0e7e2fe0a3ecda1582008b4
yujinaa <olim920@naver.com>
2022-08-09 23:17:08 +0900

    넘어갈 값이 어레이리스트로 들어오게하기

--- jsp_project/src/main/java/com/care/root/member/dao/MemberDAO.java
+++ jsp_project/src/main/java/com/care/root/member/dao/MemberDAO.java
@@ -1,8 +1,13 @@
 package com.care.root.member.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.care.root.member.dto.MemberDTO;
 
 public class MemberDAO {
 	private Connection con;
@@ -12,12 +17,29 @@ public class MemberDAO {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("드라이브 로드 성공");
-		} catch (ClassNotFoundException e) {
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "dbwls","dbwls9874");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public int getMemberList() {
+	public ArrayList<MemberDTO> getMemberList() {
 		System.out.println("getMemberLiSst 호출 되었습니다");
-		return 12345;
+		String sql = "select * from jsp_member"; //모든값 가져오기
+		ArrayList<MemberDTO> list = new ArrayList<MemberDTO>();
+		try {
+			ps = con.prepareStatement(sql);  //전송 가능한 전송객체 만들기
+			rs = ps.executeQuery();  //select는 executeQuery사용, 해당 명령어 실행
+			while(rs.next()) {
+				MemberDTO dto = new MemberDTO();
+				dto.setId(rs.getString("id"));        
+				dto.setPwd(rs.getString("pwd"));
+				dto.setName(rs.getString("name"));
+				dto.setAddr(rs.getString("addr"));
+				list.add(dto);     //결과값을 list에 저장한다.
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;   //그 결과값을 리턴으로 돌려준다.
 	}
 }
--- jsp_project/src/main/webapp/member/membership.jsp
+++ jsp_project/src/main/webapp/member/membership.jsp
@@ -11,9 +11,28 @@
 <body>membership.jsp<br>
 	<c:import url="../default/header.jsp"/>
 	<jsp:useBean id="dao" class="com.care.root.member.dao.MemberDAO"/>
-	
+		<c:set var="contextPath" value = "${pageContext.request.contextPath }"/>
 	<c:set var="list" value = "${dao.getMemberList() }"/> <!-- dao의 getMemberList라는 메소드 호출 -->
 	리턴 값 : ${list }
+		<table border = "1">   
+		<tr>
+			<th>아이디</th> 	<th>비밀번호</th> 	<th>이름</th> 	<th>주소</th>
+		</tr>
+		<c:forEach var = "member" items = "${list }">  
+		<tr>
+			<td>${member.id }</td>  
+			<td>${member.pwd }</td>
+			<td>${member.name }</td>
+			<td>${member.addr }</td>
+		</tr>	
+	</c:forEach>      
+	<tr>
+		<td colspan="4">    
+		<button type="button" onclick="location.href='${contextpath}/member/member_register.jsp'">등록</button>  -> 이렇게 해야 위치이동
+		</td>
+	</tr>
+	</table>
+	
 	
 	
 	<c:import url="../default/footer.jsp"/>
