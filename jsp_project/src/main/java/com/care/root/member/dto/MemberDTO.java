9154bdf1e12a8ff5806685794c74e11d5762d5b1
yujinaa <olim920@naver.com>
2022-08-09 20:21:36 +0900

    membership페이지 만들고 경로이동

--- jsp_project/src/main/java/com/care/root/member/dto/MemberDTO.java
+++ jsp_project/src/main/java/com/care/root/member/dto/MemberDTO.java
@@ -1,5 +1,32 @@
 package com.care.root.member.dto;
 
 public class MemberDTO {
-
+	private String id;
+	private String pwd;
+	private String name;
+	private String addr;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
 }
